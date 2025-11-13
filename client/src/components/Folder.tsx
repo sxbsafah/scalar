ef22353455f04595a52234a94d402286534d865d
Khodjabache Mhamed <sxbsafah@gmail.com>
2025-11-13 23:29:05 +0100

    adding delete Dialog for user Confirmation

--- client/src/components/Folder.tsx
+++ client/src/components/Folder.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Folder as FolderIcon } from "lucide-react";
 import {
   ContextMenu,
@@ -12,6 +11,12 @@ import { api } from "../../convex/_generated/api";
 import { Id } from "convex/_generated/dataModel";
 import { toast } from "sonner";
 import { useWorkspace } from "@/hooks/useWorkspace";
+import { Dialog, DialogTrigger, DialogHeader, DialogContent, DialogDescription, DialogFooter, DialogTitle } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { Spinner } from "./ui/spinner";
+
+
 
 type FolderProps = {
   folderName: string;
@@ -33,10 +38,12 @@ const Folder = ({
   const deleteFolder = useMutation(api.folders.deleteFolderById);
   const duplicateFolder = useMutation(api.folders.duplicateFolder);
   const workspace = useWorkspace();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDeleteFolder = async () => {
     try {
-      console.log(folderId);
+      setIsDeleting(true);
       await deleteFolder({ id: folderId });
       toast.success("Folder deleted successfully", {
         position: "bottom-right",
@@ -46,6 +53,8 @@ const Folder = ({
         position: "bottom-right",
         className: "text-destructive",
       });
+    } finally {
+      setIsDeleting(false);
     }
   };
 
@@ -66,49 +75,78 @@ const Folder = ({
   };
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <button
-          className="px-4 py-2 w-full border border-border bg-card flex rounded-sm justify-between items-center  relative overflow-hidden hover:cursor-pointer hover:shadow-2xl hover:scale-[1.02] transition-all text-start"
-          onClick={handleFolderChange}
-        >
-          <div>
-            <h4 className="font-semibold text-[14px]  text-card-foreground truncate max-w-[16ch]">
-              {folderName}
-            </h4>
-            <h5 className="text-muted-foreground text-xs capitalize">
-              {videosCount} videos
-            </h5>
-          </div>
-          <FolderIcon fill={"white"} />
-          <div className="absolute blur-3xl w-[100px] h-[100px] bg-[#ddd] -top-[50px] -left-[50px] opacity-15 rounded-full"></div>
-        </button>
-      </ContextMenuTrigger>
-      <ContextMenuContent className="w-52">
-        <ContextMenuItem>
-          Rename Folder
-          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem>
-          Move Folder
-          <ContextMenuShortcut>⌘M</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={handleFolderDuplication}>
-          Duplicate
-          <ContextMenuShortcut>⌘D</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem
-          className="text-destructive"
-          onClick={handleDeleteFolder}
-          disabled={isDefault}
-        >
-          Delete
-          <ContextMenuShortcut className="text-destructive">
-            ⌘⌫
-          </ContextMenuShortcut>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <ContextMenu>
+          <ContextMenuTrigger>
+            <button
+              className="px-4 py-2 w-full border border-border bg-card flex rounded-sm justify-between items-center  relative overflow-hidden hover:cursor-pointer hover:shadow-2xl hover:scale-[1.02] transition-all text-start"
+              onClick={handleFolderChange}
+            >
+              <div>
+                <h4 className="font-semibold text-[14px]  text-card-foreground truncate max-w-[16ch]">
+                  {folderName}
+                </h4>
+                <h5 className="text-muted-foreground text-xs capitalize">
+                  {videosCount} videos
+                </h5>
+              </div>
+              <FolderIcon fill={"white"} />
+              <div className="absolute blur-3xl w-[100px] h-[100px] bg-[#ddd] -top-[50px] -left-[50px] opacity-15 rounded-full"></div>
+            </button>
+          </ContextMenuTrigger>
+          <ContextMenuContent className="w-52">
+            <ContextMenuItem>
+              Rename Folder
+              <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+            </ContextMenuItem>
+            <ContextMenuItem>
+              Move Folder
+              <ContextMenuShortcut>⌘M</ContextMenuShortcut>
+            </ContextMenuItem>
+            <ContextMenuItem onClick={handleFolderDuplication}>
+              Duplicate
+              <ContextMenuShortcut>⌘D</ContextMenuShortcut>
+            </ContextMenuItem>
+            <DialogTrigger disabled={isDefault} asChild>
+              <ContextMenuItem
+                className="text-destructive"
+                disabled={isDefault}
+              >
+                Delete
+                <ContextMenuShortcut className="text-destructive">
+                  ⌘⌫
+                </ContextMenuShortcut>
+              </ContextMenuItem>
+            </DialogTrigger>
+          </ContextMenuContent>
+        </ContextMenu>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Folder</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              Folder and remove everything associated with it.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="outline" onClick={() => setIsOpen(false)} disabled={isDeleting}>
+              Cancel
+            </Button>
+            <Button onClick={handleDeleteFolder} disabled={isDeleting}>
+              {isDeleting ? (
+                <>
+                  Deleting
+                  <Spinner/>
+                </>
+              ) : (
+                  "Delete Folder"
+              )}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
