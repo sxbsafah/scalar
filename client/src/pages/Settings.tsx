469fe8d4fe682aee789d4d9e5ee7471a3713b492
Khodjabache Mhamed <sxbsafah@gmail.com>
2025-11-25 22:43:08 +0100

    adding light theme on loader

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -9,10 +9,10 @@ const Settings = () => {
     <>
       <Title Title="Settings" subTitle="Manage Your Settings" />
       <div className="flex gap-4">
-        <button className={`${resolvedTheme === "dark" ? "border-2 border-primary rounded-md" : ""}`} onClick={() => setTheme("dark")}>
+        <button className={`hover:scale-[1.03] duration-200  ${resolvedTheme === "dark" ? "border-2 border-primary rounded-md" : ""}`} onClick={() => setTheme("dark")}>
           <img src={DarkMode}/>
         </button>
-        <button className={`${resolvedTheme === "light" ? "border-2 border-primary rounded-md" : ""}`} onClick={() => setTheme("light")}>
+        <button className={`hover:scale-[1.03] duration-200  ${resolvedTheme === "light" ? "border-2 border-primary rounded-md" : ""}`} onClick={() => setTheme("light")}>
           <img src={LightMode} />
         </button>
       </div>
